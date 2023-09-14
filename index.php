8143e2a6e6f434e006c874926aeead0425a0c9a2
Your Name <pavel9878911@gmail.com>
2023-09-14 20:45:25 +0200

    merging with DZ9

--- index.php
+++ index.php
@@ -5,14 +5,25 @@ error_reporting(E_ALL);
 
 const BASE_DIR = __DIR__;
 
+if (!session_id()) {
+    session_start();
+}
 require_once BASE_DIR . '/vendor/autoload.php';
-
 require_once BASE_DIR . '/configs/constants.php';
 
 try {
+    require_once BASE_DIR . '/configs/DB.php';
     require_once APP_DIR . 'index.php';
 
-    require_once BASE_DIR . '/configs/router.php';
+    if (!empty($_POST)) {
+        require_once APP_DIR . 'forms/controller.php';
+    } else {
+        $commonBlocks = getContent('name IN ("navigation", "footer")');
+        require_once BASE_DIR . '/configs/router.php';
+    }
+} catch (PDOException $exception) {
+    d('PDOException');
+    dd($exception->getCode() . ' - "' . $exception->getMessage() . '"');
 } catch (Exception $exception) {
     dd($exception->getCode() . ' - "' . $exception->getMessage() . '"');
 }
